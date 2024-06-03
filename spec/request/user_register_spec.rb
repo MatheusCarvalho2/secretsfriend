5fc8aebcfb48e0b3e275f160cea3526e0f143976
BrunaBallerini <brunaballerini@gmail.com>
2024-06-03 11:33:22 -0300

    Configura gem devise e login

---
+++ spec/request/user_register_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "New Padawan", type: :request do
+  describe "usuario faz novo registro" do
+    it "com sucesso" do
+      post "/users", params: {"user[email]": "padawan@email.com", "user[password]": "1153123", "user[password_confirmation]": "1153123"}
+      p response.body
+      expect(response).to have_http_status(:see_other)
+    end
+    it "com falha" do
+      post "/users"
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+  end
+
+end
