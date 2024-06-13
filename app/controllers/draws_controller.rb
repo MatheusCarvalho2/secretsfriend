a3e0c7d3ebd43701c667f11265d656bbe50074e4
MatheusCarvalho2 <matheus_carvalho492@hotmail.com>
2024-06-13 12:09:57 -0300

    create test from new draw

--- app/controllers/draws_controller.rb
+++ app/controllers/draws_controller.rb
@@ -1,7 +1,10 @@
 class DrawsController < ApplicationController
   def create
-    p current_user
-    draw = Draw.create(title: params[:title], min_value: params[:min_value], max_value: params[:max_value], date_draws: params[:date_draw], date_present: params[:date_present], description: params[:description])
-    return render json: draw, status: :created
+    draw = Draw.create(title: params[:title], min_value: params[:min_value], max_value: params[:max_value], date_draws: params[:date_draws], date_present: params[:date_present], description: params[:description])
+    if draw.save
+      return render json: draw, status: :created
+    else
+      return render json: draw.errors, status: :bad_request
+    end
   end
 end
--- app/models/draw.rb
+++ app/models/draw.rb
@@ -1,2 +1,3 @@
 class Draw < ApplicationRecord
+  validates :title, :min_value, :max_value, :date_draws, :date_present, :description, presence: true
 end
