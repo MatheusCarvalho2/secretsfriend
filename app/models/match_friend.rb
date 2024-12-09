dc0876e9cf61a5b1bea34c48d21fbc2404fed364
BrunaBallerini <bruna@webgoal.com.br>
2024-12-09 14:39:43 -0300

    Correção da relação entre participants e match_friend

--- app/models/match_friend.rb
+++ app/models/match_friend.rb
@@ -2,8 +2,6 @@
 
 class MatchFriend < ApplicationRecord # rubocop:disable Style/Documentation
   belongs_to :draw
-  has_many :first_participant, class_name: 'Draw', foreign_key: 'participant1_id', dependent: :destroy
-  has_many :second_participant, class_name: 'Draw', foreign_key: 'participant2_id', dependent: :destroy
-  has_many :participant_first, through: :first_participant, source: :participant2
-  has_many :participant_second, through: :second_participant, source: :participant1
+  belongs_to :participant1, class_name: 'Participant', foreign_key: 'participant1_id'
+  belongs_to :participant2, class_name: 'Participant', foreign_key: 'participant2_id'
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -2,6 +2,8 @@
 
 class Participant < ApplicationRecord # rubocop:disable Style/Documentation
   belongs_to :draw
-  belongs_to :participant1, class_name: 'Draw', foreign_key: 'participant1_id'
-  belongs_to :participant2, class_name: 'Draw', foreign_key: 'participant2_id'
+  has_many :first_participant, class_name: 'MatchFriend', foreign_key: 'participant1_id', dependent: :destroy
+  has_many :second_participant, class_name: 'MatchFriend', foreign_key: 'participant2_id', dependent: :destroy
+  has_many :participant_first, through: :first_participant, source: :participant2
+  has_many :participant_second, through: :second_participant, source: :participant1
 end
