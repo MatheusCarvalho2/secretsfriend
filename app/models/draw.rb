1d97a31d88139f7331c819ab90690b5a10561b25
BrunaBallerini <bruna@webgoal.com.br>
2024-12-18 13:14:06 -0300

    Corrige logica do sorteio

--- app/models/draw.rb
+++ app/models/draw.rb
@@ -5,27 +5,24 @@ class Draw < ApplicationRecord # rubocop:disable Style/Documentation
   has_many :participants
   validates :title, :min_value, :max_value, :date_draws, :date_present, :description, presence: true
 
-  def assign_secret_friends # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
-    p "participants: #{participants}"
+  def assign_secret_friends # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+    return if participants.size < 2
 
     shuffled_participants = participants.shuffle
-    p "shuffled_participants: #{shuffled_participants}"
 
-    remaining_participants = shuffled_participants.shuffle
-    p "remaining_participants: #{remaining_participants}"
+    shuffled_participants.each_with_index do |participant, index|
+      if index == (shuffled_participants.length - 1)
+        MatchFriend.create(draw_id: id, participant1_id: participant.id,
+                           participant2_id: shuffled_participants[0].id)
+      else
+        MatchFriend.create(draw_id: id, participant1_id: participant.id,
+                           participant2_id: shuffled_participants[index + 1].id)
+      end
 
-    positions = (0...(shuffled_participants.length - 1)).to_a
-
-    shuffled_participants.each do |participant|
-      position_ids = (0...(positions.length - 1)).to_a
-
-      current_positions = position_ids.first
-      position_ids.shift
-
-      next if participant.id == remaining_participants[current_positions.to_i].id
-
-      MatchFriend.create(draw_id: id, participant1_id: participant.id,
-                         participant2_id: remaining_participants[current_positions].id)
+      # O operador módulo (%) garante que o cálculo de índice não ultrapasse o tamanho da lista.
+      # Quando o índice chega ao fim da lista, ele "volta" para o início.
+      # secret_friend = shuffled_participants[(index + 1) % shuffled_participants.size]
+      # MatchFriend.create(draw_id: id, participant1_id: participant.id, participant2_id: secret_friend.id)
     end
   end
 end
