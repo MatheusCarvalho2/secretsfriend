0ad7c706bb5e7f4dd1acf704ce50f4e60645b1ea
BrunaBallerini <bruna@webgoal.com.br>
2025-01-07 20:11:06 -0300

    Apagando a controller Emails

--- spec/requests/emails_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-require 'rails_helper' # rubocop:disable Layout/EmptyLineAfterMagicComment
-
-RSpec.describe 'Emails', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
-end
