26d173f138c06e2a97ebc8e15a75546f29969a7d
BrunaBallerini <bruna@webgoal.com.br>
2025-01-07 20:03:07 -0300

    Criação da controller email

---
+++ spec/requests/emails_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+require 'rails_helper' # rubocop:disable Layout/EmptyLineAfterMagicComment
+
+RSpec.describe 'Emails', type: :request do
+  describe 'GET /index' do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+end
