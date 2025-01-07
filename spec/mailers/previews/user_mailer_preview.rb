73a3d07754c69241b8dfd39d7d0499905ea6d650
BrunaBallerini <bruna@webgoal.com.br>
2025-01-07 19:58:17 -0300

    Correção dos nomes para send_secret_friend

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -3,7 +3,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/secret_friend
-  def secret_friend
-    UserMailer.secret_friend
+  def send_secret_friend
+    UserMailer.send_secret_friend
   end
 end
