5b22e9af0f273cb886f0dfdcce102023a11db0d5
BrunaBallerini <bruna@webgoal.com.br>
2025-02-07 13:28:29 -0300

    Otimiza código da controller password

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -2,20 +2,18 @@
 
 module Users
   class PasswordsController < Devise::PasswordsController # rubocop:disable Style/Documentation
-    def create # rubocop:disable Metrics/MethodLength
-      email = params[:email]
+    def create
+      email = params[:user][:email]
       if email.blank?
         render json: { error: 'E-mail não pode ficar em branco' }, status: :unprocessable_entity
         return
       end
 
       user = User.find_by(email:)
-      if user
-        user.send_reset_password_instructions
-        render json: { message: 'E-mail de recuperação enviado' }, status: :ok
-      else
-        render json: { error: 'E-mail não encontrado' }, status: :not_found
-      end
+      return render json: { error: 'E-mail não encontrado' }, status: :not_found unless user
+
+      user.send_reset_password_instructions
+      render json: { message: 'E-mail de recuperação enviado' }, status: :ok
     end
   end
 end
